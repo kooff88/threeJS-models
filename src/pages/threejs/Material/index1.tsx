0fd6c4994b3d6dfd3fffe0087aa5f40b57770b4f
dapeng <zhangpeng101@163.com>
2020-12-06 10:22:52 +0800

    材质

--- src/pages/threejs/Material/index1.tsx
+++ src/pages/threejs/Material/index1.tsx
@@ -18,39 +18,17 @@ const Material: FC<{}> = () => {
     /**
      * 创建网格模型
      **/
-    // 立方体网格模型
-    var geometry1 = new THREE.BoxGeometry(100, 100, 100);
-    var material1 = new THREE.MeshLambertMaterial({
-      color: 0x0000ff, //材质颜色
-      transparent: true,
-      opacity: 0.5,
-    }); //材质对象Material
-    var mesh1 = new THREE.Mesh(geometry1, material1); //网格模型对象Mesh
-    scene.add(mesh1); //网格模型添加到场景中
+    // 创建立方体几何对象geometry
+    //let geometry = new THREE.SphereGeometry(60,40,40) // 球体几何体
+    var geometry = new THREE.SphereGeometry(60, 40, 40);
+    let material = new THREE.MeshLambertMaterial({
+      color: 0x00ff00,
+      wireframe: true, // 线条模式
+    }); // 材质material
 
-    // 球体网格模型
-    var geometry2 = new THREE.SphereGeometry(60, 40, 40);
-    var material2 = new THREE.MeshLambertMaterial({
-      color: 0xff00f,
-      transparent: true,
-      opacity: 0.1,
-    });
-    var mesh2 = new THREE.Mesh(geometry2, material2); //网格模型对象Mesh
-    mesh2.translateY(120); //球体网格模型沿Y轴正方向平移100
-    scene.add(mesh2);
-
-    // 圆柱网格模型
-    var geometry3 = new THREE.CylinderGeometry(50, 50, 100, 25);
-    var material3 = new THREE.MeshLambertMaterial({
-      color: 0xffff00,
-    });
-    var mesh3 = new THREE.Mesh(geometry3, material3); //网格模型对象Mesh
-    mesh3.translateX(120); //球体网格模型沿Y轴正方向平移100
-    scene.add(mesh3); //
-
-    // 辅助坐标系
-    var axisHelper = new THREE.AxisHelper(250);
-    scene.add(axisHelper);
+    // 创建网格模型对象mesh
+    let mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh); // 网格模型添加到场景中
 
     /**
      * 光源设置

2126d6e22af9ab554c3a8a2253eb6eeafa7de290
dapeng <zhangpeng101@163.com>
2020-12-11 22:36:21 +0800

    阴影

--- src/pages/threejs/Shadow/index1.tsx
+++ src/pages/threejs/Shadow/index1.tsx
@@ -18,49 +18,54 @@ const Shadow: FC<{}> = () => {
     /**
      * 创建网格模型
      **/
-    let geometry = new THREE.BoxGeometry(50, 50, 50);
-    let material = new THREE.MeshLambertMaterial({
+    var geometry = new THREE.BoxGeometry(40, 100, 40); //创建一个立方体几何对象Geometry
+    var material = new THREE.MeshLambertMaterial({
       color: 0x0000ff,
-    });
-
-    let mesh = new THREE.Mesh(geometry, material); // 网络模型对象Mesh
-    scene.add(mesh);
+    }); //材质对象Material
+    var mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+    scene.add(mesh); //网格模型添加到场景中
+    // 设置产生投影的网格模型
     mesh.castShadow = true;
+    // mesh.position.set(0,0,0)
 
-    // 创建一个平面几何体作为投影面
-    let planeGeometry = new THREE.PlaneGeometry(300, 200);
-    let planeMaterial = new THREE.MeshLambertMaterial({
+    //创建一个平面几何体作为投影面
+    var planeGeometry = new THREE.PlaneGeometry(300, 200);
+    var planeMaterial = new THREE.MeshLambertMaterial({
       color: 0x999999,
-    });
+    }); //材质对象Material
 
-    // 平面网格对象作为投影面
-    let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-    scene.add(planeMesh);
-
+    // 平面网格模型作为投影面
+    var planeMesh = new THREE.Mesh(planeGeometry, planeMaterial); //网格模型对象Mesh
+    scene.add(planeMesh); //网格模型添加到场景中
+    planeMesh.rotateX(-Math.PI / 2); //旋转网格模型
+    planeMesh.position.y = -50; //设置网格模型y坐标
+    // 设置接收阴影的投影面
     planeMesh.receiveShadow = true;
-    planeMesh.rotateX(-Math.PI / 2);
-    planeMesh.position.y = -25;
-
+    /**
+     * 光源设置
+     */
     //环境光   环境光颜色RGB成分分别和物体材质颜色RGB成分分别相乘
     var ambient = new THREE.AmbientLight(0x444444);
     scene.add(ambient); //环境光对象添加到scene场景中
 
-    //  聚光灯光源
-    let spotLight = new THREE.SpotLight(0xffffff);
-    // 设置聚光灯位置
-    spotLight.position.set(50, 90, 50);
-    // 设置聚光灯光源发散角
-    spotLight.angle = Math.PI / 6;
-    scene.add(spotLight); // 光源添加到scene
-    // 设置用于计算阴影的光源对象
-    spotLight.castShadow = true;
-    // 设置计算阴影的区域，注意包裹对象周围
-    spotLight.shadow.camera.near = 1;
-    spotLight.shadow.camera.far = 300;
-    spotLight.shadow.camera.fov = 20;
-    // 聚光光源辅助显示
-    let spotLightHelper = new THREE.SpotLightHelper(spotLight);
-    scene.add(spotLightHelper);
+    // 方向光
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    // 设置光源位置
+    directionalLight.position.set(60, 100, 40);
+    scene.add(directionalLight);
+    // 设置用于计算阴影的光源对象dd3
+    directionalLight.castShadow = true;
+    // 设置计算阴影的区域，最好刚好紧密包围在对象周围
+    // 计算阴影的区域过大：模糊  过小：看不到或显示不完整
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 300;
+    directionalLight.shadow.camera.left = -50;
+    directionalLight.shadow.camera.right = 50;
+    directionalLight.shadow.camera.top = 200;
+    directionalLight.shadow.camera.bottom = -100;
+    // 设置mapSize属性可以使阴影更清晰，不那么模糊
+    // directionalLight.shadow.mapSize.set(1024,1024)
+    console.log(directionalLight.shadow.camera);
 
     /**
      * 相机设置
